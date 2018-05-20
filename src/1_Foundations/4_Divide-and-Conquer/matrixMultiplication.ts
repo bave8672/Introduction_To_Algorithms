9d2b591c19fcd7845e508f014861e4c3f0538fc1
Ben Aves <ben.aves@ir.com>
2018-05-20 13:11:10 +1000

    wip

--- src/1_Foundations/4_Divide-and-Conquer/matrixMultiplication.ts
+++ src/1_Foundations/4_Divide-and-Conquer/matrixMultiplication.ts
@@ -1,4 +1,4 @@
-import { add, zeros } from '../../utils/matrix';
+import { add, Matrix, subtract, zeros } from '../../utils/matrix';
 
 /**
  * Naiive matrix multiplication algorithm
@@ -9,8 +9,8 @@ import { add, zeros } from '../../utils/matrix';
  *
  * PHI(n^3)
  */
-export function matMult(A: number[][], B: number[][]): number[][] {
-    const C: number[][] = [];
+export function matMult(A: Matrix, B: Matrix): Matrix {
+    const C: Matrix = [];
     for (let i = 0; i < A.length; i++) {
         C[i] = [];
         for (let j = 0; j < B[0].length; j++) {
@@ -26,17 +26,35 @@ export function matMult(A: number[][], B: number[][]): number[][] {
 /**
  * Recursive strategy (PHI(n^3))
  * for square matrices with side length n = 2k for integer k
+ *
+ * pseudocode:
+ *
+ * SQUARE-MATRIX-MULTIPLY-RECURSIVE.A; B/
+ * 1 n D A:rows
+ * 2 let C be a new n 	 n matrix
+ * 3 if n == 1
+ * 4 c11 D a11  b11
+ * 5 else partition A, B, and C as in equations (4.9)
+ * 6 C11 D SQUARE-MATRIX-MULTIPLY-RECURSIVE.A11; B11/
+ * C SQUARE-MATRIX-MULTIPLY-RECURSIVE.A12; B21/
+ * 7 C12 D SQUARE-MATRIX-MULTIPLY-RECURSIVE.A11; B12/
+ * C SQUARE-MATRIX-MULTIPLY-RECURSIVE.A12; B22/
+ * 8 C21 D SQUARE-MATRIX-MULTIPLY-RECURSIVE.A21; B11/
+ * C SQUARE-MATRIX-MULTIPLY-RECURSIVE.A22; B21/
+ * 9 C22 D SQUARE-MATRIX-MULTIPLY-RECURSIVE.A21; B12/
+ * C SQUARE-MATRIX-MULTIPLY-RECURSIVE.A22; B22/
+ * 10 return C
  */
 export function matMultRecursive(
-    A: number[][],
-    B: number[][],
+    A: Matrix,
+    B: Matrix,
     ia = 0,
     ja = 0,
     ib = 0,
     jb = 0,
     n = A.length,
-): number[][] {
-    const C: number[][] = zeros(n);
+): Matrix {
+    const C: Matrix = zeros(n);
     switch (n) {
         case 0:
             break;
@@ -126,6 +144,65 @@ export function matMultRecursive(
             add(C, C12, 0, halfN);
             add(C, C21, halfN);
             add(C, C22, halfN, halfN);
+    }
+
+    return C;
+}
+
+/**
+ * 1. Divide the input matrices A and B and output matrix C into n=2	n=2 submatrices,
+ * as in equation (4.9). This step takes ‚.1/ time by index calculation, just
+ * as in SQUARE-MATRIX-MULTIPLY-RECURSIVE.
+ * 2. Create 10 matrices S1; S2;:::;S10, each of which is n=2 	 n=2 and is the sum
+ * or difference of two matrices created in step 1. We can create all 10 matrices in
+ * ‚.n2/ time.
+ * 3. Using the submatrices created in step 1 and the 10 matrices created in step 2,
+ * recursively compute seven matrix products P1; P2;:::;P7. Each matrix Pi is
+ * n=2 	 n=2.
+ * 4. Compute the desired submatrices C11; C12; C21; C22 of the result matrix C by
+ * adding and subtracting various combinations of the Pi matrices. We can compute
+ * all four submatrices in ‚.n2/ time.
+ */
+
+export function strassensMethod(
+    A: Matrix,
+    B: Matrix,
+    ia = 0,
+    ja = 0,
+    ib = 0,
+    jb = 0,
+    n = A.length,
+): Matrix {
+    const C: Matrix = zeros(A.length);
+
+    switch (n) {
+        case 0:
+            break;
+        case 1:
+            C[0][0] = A[ia][ja] * B[ib][jb];
+            break;
+        default:
+            const halfN = n / 2;
+
+            const S1 = zeros(halfN);
+            const S2 = zeros(halfN);
+            const S3 = zeros(halfN);
+            const S4 = zeros(halfN);
+            const S5 = zeros(halfN);
+            const S6 = zeros(halfN);
+            const S7 = zeros(halfN);
+            const S8 = zeros(halfN);
+            const S9 = zeros(halfN);
+            const S10 = zeros(halfN);
+
+            add(S1, B, 0, halfN);
+            subtract(S1, B, halfN, halfN);
+
+            add(S2, A);
+            add(S2, A, 0, halfN);
+
+            add(S3, A, halfN);
+            add(S3, A, 0, halfN);
     }
 
     return C;
